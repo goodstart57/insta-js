3b3df687e87f3c881429772ec500172b45cff72e
JAESEO LEE <goodstart57@gmail.com>
2019-04-11 06:35:39 +0000

    Post 모델에 이미지 필드 추가

--- posts/models.py
+++ posts/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Post(models.Model):
     content = models.CharField(max_length=150)
+    image = models.ImageField(blank=True)
     
     __str__ = __repr__ = lambda self: self.content
